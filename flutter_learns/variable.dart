f0e5d93f29034903268e0fd9a10d30a15425e745
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-04-11 19:06:03 +0600

    variabl

--- flutter_learns/variable.dart
+++ flutter_learns/variable.dart
@@ -3,7 +3,7 @@ void main(){
    int a=2;
    int b=32;
    int c =a+b;
-   String names='dsa learning is very necessary';
+   String names='dsa learning is very necessary for';
 
   print(name);
   print(names);
