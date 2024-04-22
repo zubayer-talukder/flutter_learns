124b1eb8b0b9c35e749a1b0e3c3aaac2541e861d
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-04-22 07:40:51 +0600

    var1

--- flutter_learns/variable1.dart
+++ flutter_learns/variable1.dart
@@ -1,4 +1,9 @@
 void main(){
   var h=2343;
   // var is here a variable
+  print('h =$h');
+    // it print the value of a h
+    print('h =${h+21}');
+    // CURLE  bracket will represent the value of h+21 
+    
 }
