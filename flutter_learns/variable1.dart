5c4157a83bfbccc94618c043122f3158ee46e379
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-04-30 01:34:51 +0600

    var

--- flutter_learns/variable1.dart
+++ flutter_learns/variable1.dart
@@ -3,8 +3,8 @@ void main(){
   // var is explicitly int format
   // var  is here a variables
    print('h =$h');
-    // it print the value of a h
-    print('h =${h+21}');
+  // it print the value of a h
+print('h =${h+21}');
     // CURLE  bracket will represent the value of h+21 
     // karon {}-->> eita 1 ta er odik kaj  kora hoile oita bujai bujso
 
