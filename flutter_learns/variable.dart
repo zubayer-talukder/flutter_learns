0ea4c8341d5e6836f8ccaa57769dcb3feb9c573a
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-10 22:13:00 +0600

    var

--- flutter_learns/variable.dart
+++ flutter_learns/variable.dart
@@ -4,7 +4,7 @@ void main(){
    int b=32;
    int c =a+b;
    int as,bd,df;
-   // variable can be written by using comma did u understand??
+   // variable can be written by using comma did u understand ??
    String names='dsa  learning is very necessary for a';
 
   print(name);
