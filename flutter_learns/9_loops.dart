3b86e812da1a55b7beed0f038e0524ae5cd4a62a
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-21 10:19:18 +0600

    for Loop

--- flutter_learns/9_loops.dart
+++ flutter_learns/9_loops.dart
@@ -1,17 +1,21 @@
-void main(){
+void main() {
   //type 1: while loop
-var i= 0;
-while(i<11){
-print('$i. Zubayer');
-  i++;
-}
+  var i = 0;
+  while (i < 11) {
+    print('$i. Zubayer');
+    i++;
+  }
 
-var j=0;
+  var j = 0;
 // type 2: do while loop
-do{
-  print("$j -Loves To Eating Water");
-  j++;
-}
-while(j<11);
+  do {
+    print("$j -Loves To Eating Water");
+    j++;
+  } while (j < 11);
 
+//type 3: For loop
+var k=0;
+for(k=0;k<11;k++){
+  print(" $k. It's 100% true");
+}
 }
