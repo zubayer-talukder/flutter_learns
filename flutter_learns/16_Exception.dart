ca42dd5ddd51043462c24d1430302b6101a6ec91
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-22 02:11:19 +0600

    Exception

--- flutter_learns/16_Exception.dart
+++ flutter_learns/16_Exception.dart
@@ -1,11 +1,23 @@
 import 'dart:io';
 
 void main() {
+  /* try{
+    statements  
+    }
+                
+   catch(e){
+            statement}
+            
+            
+            in this format code will try to execute at first then if anything happned here it will go on the error section  */
   try {
     print("Enter your Birth Year : ");
     var BirthYear = num.parse(stdin.readLineSync()!);
     // stdin.readLineSync() -->> is a input taker from a user..
 
     print('Your age is ${DateTime.now().year - BirthYear} years old');
-  } catch (e) {}
+  } catch (e) {
+    print('Invalid input.');
+    print('Error Message : $e');
+  }
 }
