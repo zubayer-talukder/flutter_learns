fe464f1695853ee0031cb5a510940ccd30896371
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-21 23:00:18 +0600

    number method

---
+++ flutter_learns/13_number_methods.dart
@@ -0,0 +1,29 @@
+void main() {
+  var x = -23;
+  print('This a NUmber = $x');
+  // this prints the  value of ODD in  boolean
+  print('x.isOdd= ${x.isOdd}');
+  //this prints the  value of EVEN in  boolean
+  print('x.isEven= ${x.isEven}');
+  // this is a Finite value Checker
+  print('x.isFinite = ${x.isFinite}');
+  // this is a InFinite value Checker
+  print('x.isInfinite = ${x.isInfinite}');
+  // this is not a NUMBER checker
+  print('x.isNaN = ${x.isNaN}');
+  // this is Negative checker 
+  print('x.isNegative = ${x.isNegative}');
+  // this prints only absolute value
+  print('x.abs() = ${x.abs()}');
+  
+   var y = 32.34;
+   print('This a NUmber = $y');
+  print('y.round() = ${y.round()}');
+  // this will print 
+  print('y.floor() = ${y.floor()}');
+  print('y.truncate() = ${y.truncate()}');
+  print('y.ceil() = ${y.ceil()}');
+  print('y. = ${y.remainder(y)}');
+
+
+}
