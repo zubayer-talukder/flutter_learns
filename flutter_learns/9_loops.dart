dc789a3b962a991dfb0969f1174bcc1a70551549
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-21 10:15:41 +0600

    do while Loop

--- flutter_learns/9_loops.dart
+++ flutter_learns/9_loops.dart
@@ -1,7 +1,17 @@
 void main(){
-  
+  //type 1: while loop
 var i= 0;
 while(i<11){
 print('$i. Zubayer');
   i++;
-}}
+}
+
+var j=0;
+// type 2: do while loop
+do{
+  print("$j -Loves To Eating Water");
+  j++;
+}
+while(j<11);
+
+}
