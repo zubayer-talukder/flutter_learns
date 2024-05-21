299ef5e6d3097e1f2ffa9b8cb8a224331d83a2d6
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-22 01:18:42 +0600

    Age calculator

--- flutter_learns/15_inputs_from_user.dart
+++ flutter_learns/15_inputs_from_user.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 void main() {
   print("Enter your Birth Year : ");
-  var age =num.parse(stdin.readLineSync()!) ;
+  var BirthYear =num.parse(stdin.readLineSync()!) ;
   // stdin.readLineSync() -->> is a input taker from a user..
-  
-  print('Your age is ${2024-age} years old');
+
+  print('Your age is ${DateTime.now().year-BirthYear} years old');
 }
