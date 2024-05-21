f7698dbcfd35597c2cb1856ddd0e3977768a6770
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-21 09:41:34 +0600

    Conditional Operator

--- flutter_learns/7_conditional_operator.dart
+++ flutter_learns/7_conditional_operator.dart
@@ -1,10 +1,12 @@
 void main(){
   var a =3;
   var b=4;
-  if(a<b){
-    print('$a is Smaller');
-  }
-  else{
-    print('$b is Smaller');
-  }
+  // if(a<b){
+  //   print('$a is Smaller');
+  // }
+  // else{
+  //   print('$b is Smaller');
+  // }
+
+  a<b?print('A = $a is Smaller'):print('B = $b is Smaller');
 }
