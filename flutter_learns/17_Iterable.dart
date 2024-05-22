36cb59bd448b2f8ad0ac9ba466fab310ebf53543
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-22 10:44:09 +0600

    iterable

--- flutter_learns/17_Iterable.dart
+++ flutter_learns/17_Iterable.dart
@@ -10,7 +10,8 @@ void main() {
   print('\n');
 
   // to get a specific value. We will use this.
-  print('This is a how list provide output : $li3[2]'); // output will get --->>> 56.652
+  print(
+      'This is a how list provide output : ${li3[2]}'); // output will get --->>> 56.652
 /* 
 List: Use when you need an ordered collection and access by index.
 Set: Use when you need a collection of unique items and order does not matter.
@@ -36,17 +37,16 @@ Map: Use when you need to associate keys with values for quick lookup by keys.
   print(l4);
 
   print('\n');
-   /* to get a specific value. We will use this.
+  /* to get a specific value. We will use this.
 
    a Set is an unordered collection of unique items and does not support indexing (i.e., you cannot access elements by their position like you would with a List). Trying to access an element by index, such as l3[2], will result in an error.
 
 If you need to access elements by their position, you should convert the Set to a List first. */
   // Convert the Set to a List
-  
-List<double>l3Lists =l3.toList();
-  // Access the third element (index 2)
-print('This is how a set provides output: ${l3Lists[2]}'); // Output: 56.652
 
+  List<double> l3Lists = l3.toList();
+  // Access the third element (index 2)
+  print('This is how a set provides output: ${l3Lists[2]}'); // Output: 56.652
 
   Map<int, String> map1 = {
     1: 'zahidul',
@@ -54,7 +54,13 @@ print('This is how a set provides output: ${l3Lists[2]}'); // Output: 56.652
     3: 'Jahinur',
     4: 'Limon',
   };
-  // to access map 
+  // to access map
   print(map1[4]);
+  // for getting all the elements in  a map
 
+  print('This is providing all the value of a map');
+  for (var elements in map1.keys) {
+    print(map1[elements]);
+    
+  }
 }
