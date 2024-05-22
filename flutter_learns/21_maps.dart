55494423e3ead6a9a8fdd9bb62aa263fdce977e6
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-22 12:53:55 +0600

    Maps

--- flutter_learns/21_maps.dart
+++ flutter_learns/21_maps.dart
@@ -11,20 +11,21 @@ void main() {
     print('This is your key: $key & value : $value');
   });
 
-  print(y[7]);// this will provide the value at 7 key
+  print(y[7]); // this will provide the value at 7 key
   print(y.keys); // what are the keys are available here
   print(y.values); // what are the values are available here
 
   print(y.entries); // show all the mapps
-  y.addAll({8:"Orrange",2:"Mango",0:"Tap Water"});
-  print(y);// now addAll will add all the values at the last of the mapps
+  y.addAll({8: "Orrange", 2: "Mango", 0: "Tap Water"});
+  print(y); // now addAll will add all the values at the last of the mapps
 
   // some boolean values
-   print(y.containsKey(8));
-   print(x.containsValue('Water Melon'));
+  print(y.containsKey(8));
+  print(x.containsValue('Water Melon'));
 
-x.remove(5);// to removing an element we just need to remove only  the key value
-print(x);
-x.clear(); // clearing all the elements
-print(x);
+  x.remove(
+      5); // to removing an element we just need to remove only  the key value
+  print(x);
+  x.clear(); // clearing all the elements
+  print(x);
 }
