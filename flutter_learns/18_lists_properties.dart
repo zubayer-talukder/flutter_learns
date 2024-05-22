57f307840a0c01eea731e27523528d5ef6d99c2e
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-22 11:06:31 +0600

    list properties

--- flutter_learns/18_lists_properties.dart
+++ flutter_learns/18_lists_properties.dart
@@ -16,6 +16,12 @@ list1.add(100);// it will add 100 at the end of the list
 print(list1);// output-->>  [1, 0, 3, 4, 5, 6, 7, 8, 100]
 
 list1.addAll([10,20,30,40]); // it will all the elements after the list
-print(list1); 
+print(list1);  // output --> [1, 0, 3, 4, 5, 6, 7, 8, 100, 10, 20, 30, 40]
+
+list1.insert(3, 99); // it will insert 99 at index 3 
+print(list1); // output -->> [1, 0, 3, 99, 4, 5, 6, 7, 8, 100, 10, 20, 30, 40]
+
+list1.insertAll(6, [88,77,66,55]);// this will add all the elements at index 6 
+print(list1); // output -->> [1, 0, 3, 99, 4, 5, 88, 77, 66, 55, 6, 7, 8, 100, 10, 20, 30, 40]
 
 }
