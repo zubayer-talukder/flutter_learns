20e40254b42bf1f105203891743e339059f67b61
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-24 09:06:53 +0600

    Funtion type

--- flutter_learns/22_function_types.dart
+++ flutter_learns/22_function_types.dart
@@ -1,11 +1,26 @@
-void main(){
- var x= fun(3+54);
- print(x);
-}
-// fun(String message){
-//   print('Mesage = $message Hello !! Zubayer');
+// void main(){
+//  var x= fun(3+54);
+//  print(x);
+// }
+// // fun(String message){
+// //   print('Mesage = $message Hello !! Zubayer');
+// // }
+
+// fun(message){
+// return 'MESSAGE = $message';
 // }
 
-fun(message){
-return 'MESSAGE = $message';
+
+import 'dart:ffi';
+
+void fun1(){
+  // void means no return
+  print(3+4);
 }
+ int fun2(){
+  return 4+5;
+ }
+  void main(){
+fun1();
+print(fun2());
+  }
