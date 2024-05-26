f7a07be22a854d41a8a8600af89bf94350a86819
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-26 10:45:50 +0600

    Default optional parameter

--- flutter_learns/23_Default_optional_parameter.dart
+++ flutter_learns/23_Default_optional_parameter.dart
@@ -5,12 +5,25 @@
 // void sum(x,y){
 //   print('Sum = ${x+y}');
 // }
-void main() {
-  sumation(5);
-// if i wanna pass a single value through a function i have to set a default value, third briacket in a  function means that it has already set a default value in the function
-}
+// void main() {
+//   sumation(5);
+// // if i wanna pass a single value through a function i have to set a default value, third briacket in a  function means that it has already set a default value in the function
+// }
 
-void sumation(x, [y = 10]) {
-  // 3rd briacket means that this function has already set a default value
-  print('Sum = ${x + y}');
+// void sumation(x, [y = 10]) {
+//   // 3rd briacket means that this function has already set a default value
+//   print('Sum = ${x + y}');
+// }
+
+//optional parameter in a function
+import 'dart:html';
+
+void main() {
+  sum(3,y:42);
+  // if i wanna set a value to a optional parameter so i have to set it in the function 42 is assign in the y 
+}
+void sum(x,{y=0,z,f,r=34}){
+  // curly briacket means that it has set a optional parameter in the function
+  // i can use multiple optional parameter 
+print('Sum = ${x+y}');
 }
