a8b218c0598ab1c4306e4efeef64449a79533075
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-26 10:29:09 +0600

    Default optional parameter

--- flutter_learns/23_Default_optional_parameter.dart
+++ flutter_learns/23_Default_optional_parameter.dart
@@ -1,6 +1,16 @@
-void main(){
-sum(5, 6);
+//void main(){
+//sum(5, 6);
+// if i wanna pass a single value through a function i have to set a default value
+//}
+// void sum(x,y){
+//   print('Sum = ${x+y}');
+// }
+void main() {
+  sumation(5);
+// if i wanna pass a single value through a function i have to set a default value
 }
-void sum(x,y){
-  print('Sum = ${x+y}');
+
+void sumation(x, [y = 10]) {
+  // 3rd briacket means that this function has alredy set a default value
+  print('Sum = ${x + y}');
 }
