555828616ce28d482a32e19aafd9dd63c3644123
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-27 08:56:56 +0600

    Array printing just

---
+++ flutter_learns/24_passing_array_through_function.dart
@@ -0,0 +1,13 @@
+void main(){
+  var a =[23,42,6.33,85,'Zubayer',true];
+  var b =[23,4.52,63,85,'Abdullah',false];
+
+  // type 1: printing arrary in a simple way
+  print(a);
+
+  //type 2: printing arrary by using for loop
+  print('The value of a arrary by using for loop');
+  for(int i=0;i<a.length;i++){
+    print('Value ${ i+1} = ${ a[i]}');
+  }
+}
