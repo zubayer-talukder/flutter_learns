8615832c49403237bc97b35a6d5cfdc99ebab048
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-29 00:45:15 +0600

    FormatException

--- flutter_learns/26_through_exception.dart
+++ flutter_learns/26_through_exception.dart
@@ -5,7 +5,14 @@ void main() {
 void info() {
   for (var i = 0; i <= 10; i++) {
     if (i == 6) {
-      throw 'Error';
+// type 1:
+
+      // throw 'Error';
+//type 2:
+      //  throw Exception('Error');
+
+//type 3:
+      throw FormatException();
     }
     print('i=$i');
   }
