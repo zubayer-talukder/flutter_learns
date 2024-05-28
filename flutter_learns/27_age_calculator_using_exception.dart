e87dddcaaa783cd3d40ccd774000fc350ed80e46
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-29 01:27:24 +0600

    Age Calculator by using Try Catch & using Function

--- flutter_learns/27_age_calculator_using_exception.dart
+++ flutter_learns/27_age_calculator_using_exception.dart
@@ -1,20 +1,24 @@
 import 'dart:io';
 
 void main() {
+  age_calculator();
+}
+
+void age_calculator() {
   while (true) {
     try {
       print('Enter Your Birth Day Year: ');
       var birth_year = stdin.readLineSync()!;
       var age = DateTime.now().year - num.parse(birth_year);
-      print(age);
+      // print(age);
       if (age > 150) throw FormatException();
-      if (age < 6) throw Exception;
+      if (age < 0) throw Exception;
       print("Your age is $age year's old");
       break;
     } on FormatException {
       print('Invalid data.....try again');
     } catch (e) {
-      print("Your not 18+ old");
+      print("Your Underage people my boy!!");
     }
   }
 }
