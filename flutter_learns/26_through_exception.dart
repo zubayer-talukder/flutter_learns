d6088512b7ec16de4037dbb0b93a47b221071691
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-05-29 00:56:16 +0600

    try catch  useage in a function

--- flutter_learns/26_through_exception.dart
+++ flutter_learns/26_through_exception.dart
@@ -1,25 +1,43 @@
-void main() {
-  // info();
+// void main() {
+//   // info();
 
-  try {
-    info();
-  } catch (e) {
-    print('Why You Have Entered me 5');
-  }
+//   try {
+//     info();
+//   } catch (e) {
+//     print('Why You Have Entered me?? i== 6\nShame on you Bro!!');
+//   }
+// }
+
+// void info() {
+//   for (var i = 0; i <= 10; i++) {
+//     if (i == 6) {
+
+// // type 1:
+
+//       // throw 'Error';
+// //type 2:
+//       //  throw Exception('Error');
+
+// //type 3:
+//       throw FormatException();
+//     }
+//     print('i=$i');
+//   }
+// }
+
+void main() {
+  info();
 }
 
 void info() {
-  for (var i = 0; i <= 10; i++) {
-    if (i == 6) {
-// type 1:
-
-      // throw 'Error';
-//type 2:
-      //  throw Exception('Error');
-
-//type 3:
-      throw FormatException();
+  try {
+    for (var i = 0; i <= 10; i++) {
+      if (i == 6) {
+        throw FormatException();
+      }
+      print('i=$i');
     }
-    print('i=$i');
+  } catch (e) {
+    print(e);
   }
 }
