754e9f18480367a28506dc881f09dea907f2348a
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-01 07:06:09 +0600

    Lamda Expressions

--- flutter_learns/28_Lamda_expression.dart
+++ flutter_learns/28_Lamda_expression.dart
@@ -1,10 +1,28 @@
-fun1() {
+void fun1() {
   print(40 + 60);
 }
-// lamada function
 
-lam1() => 3 + 4;
+int fun2() {
+  return 3 + 4;
+}
+
+void fun3(x, y) {
+  print(x + y);
+}
+
+int fun4(x, y) {
+  return x + y;
+}
+// lamada function
+//  it means that lamda is a type of f
+void lam1() => print(40 + 60);
+int lam2() => 50 + 60;
+void lam3(x,y) =>print(x+y);
+int lam4(x,y) => x + y;
+
 void main() {
-  fun1();
   lam1();
+  lam2();
+  lam3(32, 31);
+  print(lam4(99,88));
 }
