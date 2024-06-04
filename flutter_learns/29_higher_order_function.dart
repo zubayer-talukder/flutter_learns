51a9b4ee85a8825e6f3aac7fa403f3d5710c8908
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-04 15:20:11 +0600

    higher Order Functions

--- flutter_learns/29_higher_order_function.dart
+++ flutter_learns/29_higher_order_function.dart
@@ -1,3 +1,12 @@
-void main(){
-  
+void sum(x, y) {
+  print('Sum from regular function = ${x + y}');
 }
+
+var a1 = (x, y) => print('Sum from lamda = ${x + y}');
+
+void main() {
+  sum(32, 55);
+  a1(55, 66);
+}
+
+void HOf() {}
