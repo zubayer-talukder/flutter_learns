f3cda11c132f1e6818bca75128bf88da963af2fe
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-08 13:08:22 +0600

    Where fun

--- flutter_learns/30_where_functions.dart
+++ flutter_learns/30_where_functions.dart
@@ -1,6 +1,7 @@
 void main() {
-  var a1 = [21, 22, 23, 24, 25];
+  var a1 = [21, 22, 23, 24, 25, 43];
   print('a1 =$a1');
-  var b1 = a1.where((element) => true).toList();
+  var b1 = a1.where((element) => element < 25).toList();
+
   print('b1 =$b1');
 }
