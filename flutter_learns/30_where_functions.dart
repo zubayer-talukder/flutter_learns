47eda8aaee76eabed29dd23f4e5bdacd279facb9
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-09 19:27:58 +0600

    Where functionss

--- flutter_learns/30_where_functions.dart
+++ flutter_learns/30_where_functions.dart
@@ -2,6 +2,6 @@ void main() {
   var a1 = [21, 22, 23, 24, 25, 43];
   print('a1 =$a1');
   var b1 = a1.where((element) => element < 25).toList();
-
+// all the condition that satisfy the condition ..(element<25)  this will be execute
   print('b1 =$b1');
 }
