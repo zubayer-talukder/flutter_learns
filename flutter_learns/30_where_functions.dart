52bd6254fecddb5a871d1d87e7805957e2ca2e20
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-11 13:37:25 +0600

    Where function final

--- flutter_learns/30_where_functions.dart
+++ flutter_learns/30_where_functions.dart
@@ -1,18 +1,23 @@
 void main() {
-  var a1 = [21, 22, 23, 24, 25, 43];
+  var a1 = [21, 22, 22.99, 23.43, 23, 24, 25, 43, 99.4];
   print('a1 =$a1');
 
 // all the condition that satisfy the condition ..(element<25)  this will be filtered
 
   var b1 = a1.where((element) => element < 25).toList();
   // this will print all the element where less than 25
-  var b2 = a1.where((element) => element < 23).toList();
-  var b3 = a1.firstWhere((element) => element % 2 == 0, orElse: () => 0);
+
+  var b2 = a1.firstWhere((element) => element % 2 == 0, orElse: () => 0);
   // this will print the first element only that satisfy the condition
-  var c1 = a1.lastWhere((element) => element % 2 == 0, orElse: () => 0);
+  var b3 = a1.lastWhere((element) => element % 2 == 0, orElse: () => 0);
   //this will print the last element only that satisfy the condition
-  var c2 = a1.where((element) => element < 23).toList();
-  var d = a1.where((element) => element < 23).toList();
+
+  var c1 = a1.indexWhere((element) => element % 2 == 0, 5);
+  // this will print the index of even element and 24 specifically
+  var c2 = a1.lastIndexWhere((element) => element % 2 == 1);
+  // last element of an odd number
+  var d = a1.whereType<double>();
+  // this will print the only double type data from a  list
 
   print('b1 =$b1');
   print('b2 =$b2'); //
