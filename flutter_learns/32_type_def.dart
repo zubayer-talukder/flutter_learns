9e6945437e8775fa74572c2059defcd0c2bedf3c
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-15 18:29:52 +0600

    Type def

---
+++ flutter_learns/32_type_def.dart
@@ -0,0 +1,28 @@
+// higher order functions
+void calc(x, y, Function s) {
+  s(x, y);
+}
+
+void main() {
+  calc(5, 7, sub); // this will call sub() function
+  // sub(5,7);
+  // we can pass any function by using higher order function
+
+  calc(9, 20, mul);// this will call multiplication function by using higher order function 
+}
+
+void sum(a, b) {
+  print('$a + $b =${a + b}');
+}
+
+void sub(a, b) {
+  print('$a - $b =${a - b}');
+}
+
+void mul(a, b) {
+  print('$a * $b =${a * b}');
+}
+
+void div(a, b) {
+  print('$a / $b =${a / b}');
+}
