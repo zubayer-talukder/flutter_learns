63e5692579fb426988cfabad920820655f4b4a78
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-17 13:28:57 +0600

    class is here

---
+++ flutter_learns/oop/1_class_object.dart
@@ -0,0 +1,13 @@
+void main(){
+
+}
+class Human{
+  String name ="Zubayer";
+  int age =22;
+  double height = 170.4;
+// the function inside a class 
+  void fun(){
+    age+=5;
+    print('age =$age');
+  }
+}
