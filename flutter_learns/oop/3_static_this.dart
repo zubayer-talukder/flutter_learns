4a818303589259f42d1c396730d993da27bf14b6
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-19 21:46:04 +0600

    This Keyword

--- flutter_learns/oop/3_static_this.dart
+++ flutter_learns/oop/3_static_this.dart
@@ -1,18 +1,18 @@
-
-
 void main() {
-  var h1 = Human('Abdullah', 22, 170);
-  var h2 = Human('Zubayer', 26, 169);
+  var h1 = Human();
+  h1.values('Abdullah', 22, 170);
+  var h2 = Human();
+  h2.values('Zubayer', 26, 169);
 }
 
 class Human {
   var name, age, height;
-  Human(String name, int age, double height){
-  this.name =name;
-  this.age=age;
-  this.height=height;
-  print('Name =$name');
-  print('Age = $age');
+  void values(String name, int age, double height) {
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    print('Name =$name');
+    print('Age = $age');
+    print('Height = $height');
   }
-
 }
