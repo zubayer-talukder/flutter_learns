e5719e595294eb71e99c27b1f267db724d399119
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-19 21:58:52 +0600

    Static keyword added

--- flutter_learns/oop/3_static_this.dart
+++ flutter_learns/oop/3_static_this.dart
@@ -1,19 +1,48 @@
+// //void main() {
+//   var h1 = Human();
+//   h1.values('Abdullah', 22, 170);
+//   var h2 = Human();
+//   h2.values('Zubayer', 26, 169);
+// }
+
+// class Human {
+//   var name, age, height;
+//   void values(String name, int p_age, double height) {
+//     this.name = name;
+//     age = p_age; // it can be written like this type
+//     this.height = height;
+//     print('Name = $name');
+//     print('Age = $age');
+//     print('Height = $height');
+//     print("----------------");
+//     age +=5;
+//     print('New age = $age');
+//     print('Lenght = $height');
+//   }
+// }
+
+
+// static variables is here 
 void main() {
-  var h1 = Human();
-  h1.values('Abdullah', 22, 170);
-  var h2 = Human();
-  h2.values('Zubayer', 26, 169);
+  //var h1 = Human();// no need this 
+  Human.values('Abdullah', 22, 170);
+  // var h2 = Human();
+ Human.values('Zubayer', 26, 169);
 }
 
 class Human {
-  var name, age, height;
-  void values(String name, int p_age, double height) {
-    this.name = name;
-    age = p_age;
-    this.height = height;
+  static var name, age, height;
+ static void values(String name, int p_age, double height) {
+    name = name;
+    age = p_age; 
+    height = height;
     print('Name = $name');
     print('Age = $age');
     print('Height = $height');
+    
+    age +=5;
+    print('New age = $age');
+    print('Lenght = $height');
     print("----------------");
   }
 }
