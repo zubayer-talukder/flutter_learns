a8d579a5fea04107061517a0712cdcf72f7db37c
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-20 09:45:22 +0600

    late keyword use

--- flutter_learns/oop/4_setter&Getter.dart
+++ flutter_learns/oop/4_setter&Getter.dart
@@ -1,19 +1,21 @@
-void main(){
+void main() {
   var h1 = Human();
   h1.set_age(89);
- 
- print(h1.getAge()) ;
 
+  print(h1.getAge());
 }
-class Human{
-num age =0 ;
-  // set method is for setting a value for a variable 
-  void set_age(num p_age){
+
+class Human {
+  num age = 0;
+// if we don't assign the value compiler will give us error so we have to use "late" keyword --> it means that 
+
+  // set method is for setting a value for a variable
+  void set_age(num p_age) {
     age = p_age;
   }
   // get method is for retrive a value from a values
 
-  num getAge(){
+  num getAge() {
     return age;
   }
 }
