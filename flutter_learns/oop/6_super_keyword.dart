743a62aed836199ae9d22634b0a7663674dcc8ae
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-24 05:19:35 +0600

    git ignore

--- flutter_learns/oop/6_super_keyword.dart
+++ flutter_learns/oop/6_super_keyword.dart
@@ -1,22 +1,18 @@
 //indirect super class
 class a {
-  void info(msg)=> print(' $msg Zubayer.');
+  void info(msg) => print(' $msg Zubayer.');
 }
 
 // direct super class
-class b  extends a{
-void fun()=>super.info("Abdullah");
+class b extends a {
+  void fun() => super.info("Abdullah");
 // "super"--> it means that info function is calling from class a. class b is calling from its upper or super class a . that's why we use 'Super' keyword.
 }
 
 //sub class
-class c extends b{
-
-}
+class c extends b {}
 
 void main() {
-
-
-  var z =c();
+  var z = c();
   z.fun();
 }
