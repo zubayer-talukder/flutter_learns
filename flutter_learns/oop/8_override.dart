fa777eac37867cbced867781d99c5161e674c69a
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-25 06:42:16 +0600

    override

---
+++ flutter_learns/oop/8_override.dart
@@ -0,0 +1,51 @@
+
+// class a {
+//   void info()=> print('1- Hi!! Zubayer.');
+// }
+
+// class b  extends a{
+// void info()=> print('2- Hi!! Zubayer.');
+// }
+
+// class c extends b{
+// void info()=> print('3- Hi!! Zubayer.');
+// }
+
+// void main() {
+//   var x = a();
+//   x.info();
+
+//   var y = b(); 
+//   y.info();
+
+//   var z =c();
+//    z.info();
+// }
+
+
+
+// overriding Examples
+
+class father {
+  // parent class
+  TotalTaka() {
+    print('Total ammount father = 90000');
+  }
+}
+
+class son extends father {
+  // child class
+   TotalTaka() {
+    print('Total ammount son = 80000');
+  }
+}
+
+void main() {
+  var SonObj = son();
+  SonObj.TotalTaka();
+  // eibar jehetu son ar father er moddhe same nam er function ase tai jokun son nam er obj toiri kora hobe tokun son er kase jeita ase oita sudu  print korbe . eita ke bole overriding.
+
+  var fatherObj = father();
+  fatherObj.TotalTaka();
+  // parent can access total taka as well as child  can also access it .
+}
