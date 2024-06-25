55ae9eb49b010bcc116f022cc00cd071d0e5fb52
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-25 06:34:04 +0600

    inheritance father & son Example

--- flutter_learns/oop/5_inheritance2.dart
+++ flutter_learns/oop/5_inheritance2.dart
@@ -1,17 +1,20 @@
-class father{
+class father {
   // parent class
-  TotalTaka(){
-
-print('Total ammount = 90000');
+  TotalTaka() {
+    print('Total ammount = 90000');
   }
-  
 }
-class son extends father{
- // child class
-}
-void main(){
- var SonObj = son();
- SonObj.TotalTaka();
- // i am accessing father er function from son
 
+class son extends father {
+  // child class
+}
+
+void main() {
+  var SonObj = son();
+  SonObj.TotalTaka();
+  // i am accessing father er function from son.
+
+  var fatherObj = father();
+  fatherObj.TotalTaka();
+  // parent can access total taka as well as child  can also access it .
 }
