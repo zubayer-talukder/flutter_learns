144f734cb84c2cc5ce63bad2f03825d2dbff9cea
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-29 07:51:57 +0600

    Generic

--- flutter_learns/oop/13_generic.dart
+++ flutter_learns/oop/13_generic.dart
@@ -1,19 +1,31 @@
-void main(){
-  var a = A();
-a.x=24;
-a.y=3.14;
-print(a.x);
-print(a.y);
 
-var b = A();
+void main(){
+  var a = A<int,double>();
 a.x=24;
 a.y=3.14;
+
 print(a.x);
 print(a.y);
+print(a.info(a.x,a.y));
+
+
+var b = A<String,int>();
+b.x="Abdullah Zubayer Talukder";
+b.y=57;
+print (b.info("Md. abdul Bari Talukder",57));
+print(b.x);
+print(b.y);
 
 }
-class A{
-late int x;
-late double y;
+
+// generic class means that the type of class is not specified yet. we will assign the  type when we are creating a object from a classes 
+
+class A <t,e>{
+late t x;
+late e y;
+// we can use generic in a function also 
+info(t p, e q){
+ return [p,q];
+}
 }
 
