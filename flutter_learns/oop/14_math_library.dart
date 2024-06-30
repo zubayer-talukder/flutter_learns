5c62138039e26fc4394b07b250f9937e3daada08
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-30 08:23:56 +0600

    natural logarithm added

--- flutter_learns/oop/14_math_library.dart
+++ flutter_learns/oop/14_math_library.dart
@@ -4,10 +4,19 @@ void main() {
   print('sin(45)=${sin(45) * (pi / 180)}');
   print('cos(0)=${cos(0 * (pi) / 180)}');
   print('tan(45)=${tan(45 * (pi / 180))}');
-  // inverse function 
-  print('asin(0.5)=${asin(0.5)*(180/pi)}');
-  print('acos(1)=${acos(1)*(180/pi)}');
-  print('atan(1)=${atan(1)*(180/pi)}');
+  // inverse function
+  print('asin(0.5)=${asin(0.5) * (180 / pi)}');
+  print('acos(1)=${acos(1) * (180 / pi)}');
+  print('atan(1)=${atan(1) * (180 / pi)}');
+  print('exp(5)=${exp(5)}');
+  print('log(4)=${log(4)}');
+  print('pow(3,5)=${pow(3, 5)}');
+  print('sqrt(125)=${sqrt(125)}');
+  print('max(3,5.342)=${max(3, 5.342)}');
+  print('min(3,5.342)=${min(3, 5.342)}');
+  print('e=$e');
+  print('In2=$ln2');
   
+
 
 }
