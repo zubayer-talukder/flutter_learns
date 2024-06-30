207be4aa41df51b321b75f2c7935a06b7914fefc
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-30 08:05:05 +0600

    tan added

--- flutter_learns/oop/14_math_library.dart
+++ flutter_learns/oop/14_math_library.dart
@@ -1,9 +1,8 @@
 import 'dart:math';
-void main(){
 
-print('sin(45)=${sin(45)*(pi/180)}');
-print('cos(0)=${cos(0*(pi)/180)}');
-
-
-
+void main() {
+  print('sin(45)=${sin(45) * (pi / 180)}');
+  print('cos(0)=${cos(0 * (pi) / 180)}');
+  print('tan(45)=${tan(45 * (pi / 180))}');
+  // inverse function 
 }
