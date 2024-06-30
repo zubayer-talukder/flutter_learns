38d55a2e3438fcb4341b005d38d37531e5ce06d3
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-06-30 08:15:13 +0600

    inverse tan added

--- flutter_learns/oop/14_math_library.dart
+++ flutter_learns/oop/14_math_library.dart
@@ -5,4 +5,9 @@ void main() {
   print('cos(0)=${cos(0 * (pi) / 180)}');
   print('tan(45)=${tan(45 * (pi / 180))}');
   // inverse function 
+  print('asin(0.5)=${asin(0.5)*(180/pi)}');
+  print('acos(1)=${acos(1)*(180/pi)}');
+  print('atan(1)=${atan(1)*(180/pi)}');
+  
+
 }
