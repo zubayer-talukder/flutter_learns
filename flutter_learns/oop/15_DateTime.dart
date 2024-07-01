d7daaf93a8205fb35be5f82758a0d718c300e81f
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-07-01 06:52:35 +0600

    Date Complete

--- flutter_learns/oop/15_DateTime.dart
+++ flutter_learns/oop/15_DateTime.dart
@@ -11,6 +11,19 @@ void main(){
   print(DateTime.august);//8
   var date = DateTime.utc(2024,7,01,6,30);
   print(date);
-  
+  var date2 = DateTime.utc(2024,DateTime.july,01);
+  print(date2);
+  var date3 = DateTime.parse('2002-07-28 16:35:04');
+  print(date3);
+  print(date.isUtc);
+  print(date3.isUtc);
+  print(date.day);
+  print(date3.weekday);
+  print(date3.hour);
+  print(date2.isAfter(date3));
+  print(date3.isBefore(date));
+
+  var diff= now.difference(date3);
+  print(diff.inDays);
 
 }
