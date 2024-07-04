67ffb01ac43301f535d95008941e60dc34af5eab
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-07-04 06:51:12 +0600

    Lexical Closure

--- flutter_learns/oop/18_lexical_closure.dart
+++ flutter_learns/oop/18_lexical_closure.dart
@@ -1,7 +1,12 @@
 // Lexical Closure
 void main(){
- void fun(String str){
+//  void fun(String str){
+//     print('Message $str');
+//   }
+//   fun('Hello');
+
+ var a = (String str){
     print('Message $str');
-  }
-  fun('Hello');
+  };
+ a('Hello');
 }
