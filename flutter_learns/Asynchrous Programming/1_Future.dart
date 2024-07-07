43c0b92f8b47dcd456df29bd511a80883e8d0870
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-07-07 14:47:39 +0600

    Future Keyword

--- flutter_learns/Asynchrous Programming/1_Future.dart
+++ flutter_learns/Asynchrous Programming/1_Future.dart
@@ -1,6 +1,20 @@
+import 'dart:math';
+
 void main() {
-  final f1 = Future(info); 
-  f1.then((value) => print(value));
+//   final f1 = Future(info); 
+//   f1.then((value) => print(value));
+
+final f1 = Future(info);
+f1.then((value) => print(value)).catchError((e)=>print(e));
+
+
+final f2=Future.value(2).then((value) => print(value));
+
+
+final f3=Future.delayed(Duration(seconds: 10),()=>print(99));
+// this will give us result after 10 seconds of compilation of the program
+
+
   print(1);// it will complete the rest of code at first then it will go on the 'Future' keyword block code . it's an example of asynchonus programming .
 }
 
